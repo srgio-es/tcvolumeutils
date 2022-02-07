887c247b2c658e5ca29ec6059a893ccd09c03775
Sergio Soto <me@srgio.es>
2022-02-07 14:47:11 +0100

    chore(refactor): Clean unused code and comments

--- cmd/root.go
+++ cmd/root.go
@@ -5,8 +5,6 @@ import (
 )
 
 var (
-	cfgFile string
-
 	logFolder  string
 	logFile    string
 	reportFile string
@@ -19,45 +17,10 @@ var rootCmd = &cobra.Command{
 	Long:  "Teamcenter Volume management utils is a set of tools that will help Teamcenter administrator to perform some TC volumes management tedious tasks in an easier way.",
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	cobra.CheckErr(rootCmd.Execute())
 }
 
 func init() {
-	// cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tcvolumeutils.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.PersistentFlags().BoolP("verbose", "v", false, "increase verbosity for commands")
 }
-
-// // initConfig reads in config file and ENV variables if set.
-// func initConfig() {
-// 	if cfgFile != "" {
-// 		// Use config file from the flag.
-// 		viper.SetConfigFile(cfgFile)
-// 	} else {
-// 		// Find home directory.
-// 		home, err := homedir.Dir()
-// 		cobra.CheckErr(err)
-
-// 		// Search config in home directory with name ".tcvolumeutils" (without extension).
-// 		viper.AddConfigPath(home)
-// 		viper.SetConfigName(".tcvolumeutils")
-// 	}
-
-// 	viper.AutomaticEnv() // read in environment variables that match
-
-// 	// If a config file is found, read it in.
-// 	if err := viper.ReadInConfig(); err == nil {
-// 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-// 	}
-// }
